4785391f8d8b0b89171a446db553e1a0b0bb3950
GreybeardGM <webmaster@sector-001.de>
2025-06-17 13:48:58 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-import { convertStress } from "./convert-stress.js";
+import { convertStress } from "./scripts/convert-stress.js";
 
 Hooks.once("ready", () => {
+  // Global registry for use in macros
   game.moshGreybeardQol = game.moshGreybeardQol || {};
   game.moshGreybeardQol.convertStress = convertStress;
 });
