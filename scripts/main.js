7ec2915260d2bdf8e7bbefaf199cb7a7f338d6b6
GreybeardGM <webmaster@sector-001.de>
2025-06-17 13:55:44 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ Hooks.once("ready", () => {
   // Global registry for use in macros
   game.moshGreybeardQol = game.moshGreybeardQol || {};
   game.moshGreybeardQol.convertStress = convertStress;
-  
+
+  // Debug Check
   console.log("✅ MoSh Greybearded QoL loaded");
+  console.log("✅ convertStress function:", convertStress);
 });
