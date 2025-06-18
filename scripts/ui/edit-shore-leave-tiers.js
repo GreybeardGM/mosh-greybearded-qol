c3e243eddce93e867d7bdb6c1e54edb5792fe38d
GreybeardGM <webmaster@sector-001.de>
2025-06-18 11:05:02 +0200

    Update edit-shore-leave-tiers.js

--- scripts/ui/edit-shore-leave-tiers.js
+++ scripts/ui/edit-shore-leave-tiers.js
@@ -16,6 +16,16 @@ export class ShoreLeaveTierEditor extends FormApplication {
     return { tiers: foundry.utils.deepClone(tiers) };
   }
 
+  activateListeners(html) {
+    super.activateListeners(html);
+    html.find(".reset-defaults").on("click", async () => {
+      const module = await import("../config/default-shore-leave-tiers.js");
+      await game.settings.set("mosh-greybearded-qol", "shoreLeaveTiers", module.SHORE_LEAVE_TIERS);
+      this.render();
+      ui.notifications.info("Shore Leave tiers reset to defaults.");
+    });
+  }
+
   async _updateObject(event, formData) {
     const data = foundry.utils.expandObject(formData);
     await game.settings.set("mosh-greybearded-qol", "shoreLeaveTiers", data.tiers);
