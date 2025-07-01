8145338072e80f729fcce5dc4fa650d9f80c4eba
GreybeardGM <webmaster@sector-001.de>
2025-07-01 17:22:33 +0200

    Update get-theme-color.js

--- scripts/utils/get-theme-color.js
+++ scripts/utils/get-theme-color.js
@@ -1,5 +1,7 @@
 
 export function getThemeColor() {
-  const theme = game.settings.get("mosh-greybearded-qol", "themeColor") || "orange";
+  const override = game.settings.get("mosh-greybearded-qol", "themeColorOverride");
+  const global = game.settings.get("mosh-greybearded-qol", "themeColor");
+  const theme = override || global || "orange";
   return `theme-${theme}`;
 }
