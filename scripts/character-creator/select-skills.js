5c742d9350e33afcda26baf23dc228b71e3fd587
GreybeardGM <webmaster@sector-001.de>
2025-10-17 14:43:52 +0200

    Update select-skills.js

--- scripts/character-creator/select-skills.js
+++ scripts/character-creator/select-skills.js
@@ -72,7 +72,7 @@ export async function selectSkills(actor, selectedClass) {
     };
   });
 
-  const html = await renderTemplate("modules/mosh-greybearded-qol/templates/character-creator/select-skills.html", {
+  const html = await foundry.applications.handlebars.renderTemplate("modules/mosh-greybearded-qol/templates/character-creator/select-skills.html", {
     themeColor: getThemeColor(),
     actor,
     selectedClass,
