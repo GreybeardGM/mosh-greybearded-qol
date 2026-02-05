8ee6acaaeab50b6585751e92bc5a17ca0447b936
GreybeardGM <webmaster@sector-001.de>
2026-02-05 23:43:19 +0100

    Update dice-so-nice.js

--- scripts/dice-so-nice.js
+++ scripts/dice-so-nice.js
@@ -1,12 +1,16 @@
-const D_0_TO_4 = ["1","2","3","4","0","1","2","3","4","0"];
-const D_0_TO_9 = ["1","2","3","4","5","6","7","8","9","0"];
-const D_00_TO_90 = ["10","20","30","40","50","60","70","80","90","00"];
+const D_0_TO_4_LABELS = ["1","2","3","4","0"];
+const D_0_TO_4_VALUES = [1,2,3,4,0];
+const D_0_TO_9_LABELS = ["1","2","3","4","5","6","7","8","9","0"];
+const D_0_TO_9_VALUES = [1,2,3,4,5,6,7,8,9,0];
+const D_00_TO_90_LABELS = ["10","20","30","40","50","60","70","80","90","00"];
+const D_00_TO_90_VALUES = [10,20,30,40,50,60,70,80,90,00];
 
-function addZeroBasedPreset(dice3d, { type, labels, system, shape }) {
+function addZeroBasedPreset(dice3d, { type, labels, values, system, shape }) {
   dice3d.addDicePreset(
     {
       type,
       labels,
+      values,
       system
     },
     shape
@@ -25,9 +29,9 @@ export function registerDiceSoNice() {
     dice3d.addSystem(system, true);
 
     // scripts/dice-so-nice.js
-    addZeroBasedPreset(dice3d, { type: "dx", labels: D_0_TO_9, system: system.id, shape: "d10" });
-    addZeroBasedPreset(dice3d, { type: "dh", labels: [D_00_TO_90, D_0_TO_9], system: system.id, shape: "d100" });
-    addZeroBasedPreset(dice3d, { type: "dv", labels: D_0_TO_4, system: system.id, shape: "d10" });
+    addZeroBasedPreset(dice3d, { type: "dx", labels: D_0_TO_9_LABELS, values: D_0_TO_9_VALUES, system: system.id, shape: "d10" });
+    addZeroBasedPreset(dice3d, { type: "dh", labels: D_00_TO_90_LABELS, values: D_00_TO_90_VALUES, system: system.id, shape: "d100" });
+    addZeroBasedPreset(dice3d, { type: "dv", labels: D_0_TO_4_LABELS, values: D_0_TO_4_VALUES, system: system.id, shape: "d10" });
     
   });
 }
