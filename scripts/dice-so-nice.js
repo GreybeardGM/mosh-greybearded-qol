e2f0d691426b17a191eace6179857f6e782526e8
GreybeardGM <webmaster@sector-001.de>
2026-02-05 19:21:36 +0100

    Update dice-so-nice.js

--- scripts/dice-so-nice.js
+++ scripts/dice-so-nice.js
@@ -5,6 +5,7 @@ const ZERO_TO_NINETY_NINE = Array.from({ length: 100 }, (_, index) =>
 const ZERO_TO_FOUR_DUPLICATED = Array.from({ length: 10 }, (_, index) =>
   String(Math.floor(index / 2))
 );
+const DV_0_TO_4x2 = ["0","0","1","1","2","2","3","3","4","4"];              // 10 Faces
 
 function addZeroBasedPreset(dice3d, { type, labels, system, shape }) {
   dice3d.addDicePreset(
@@ -29,9 +30,9 @@ export function registerDiceSoNice() {
     dice3d.addSystem(system, true);
 
     // scripts/dice-so-nice.js
-    addZeroBasedPreset(dice3d, { type: "dx", labels: ZERO_TO_NINE, system: system.id, shape: "d10" });
-    addZeroBasedPreset(dice3d, { type: "dh", labels: ZERO_TO_NINETY_NINE, system: system.id, shape: "d100" });
-    addZeroBasedPreset(dice3d, { type: "dv", labels: ZERO_TO_FOUR_DUPLICATED, system: system.id, shape: "d10" });
+    addZeroBasedPreset(dice3d, { type: "dx", system: system.id, shape: "d10" });
+    addZeroBasedPreset(dice3d, { type: "dh", system: system.id, shape: "d100" });
+    addZeroBasedPreset(dice3d, { type: "dv", labels: DV_0_TO_4x2, system: system.id, shape: "d10" });
     
   });
 }
