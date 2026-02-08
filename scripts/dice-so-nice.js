3ce8339541aceefa6816597176a5a74933f74736
GreybeardGM <webmaster@sector-001.de>
2026-02-08 02:11:22 +0100

    Update dice-so-nice.js

--- scripts/dice-so-nice.js
+++ scripts/dice-so-nice.js
@@ -1,6 +1,6 @@
 const D_0_TO_4_LABELS = ["0","1","2","3","4","0","1","2","3","4"];
 const D_0_TO_9_LABELS = ["0","1","2","3","4","5","6","7","8","9"];
-const D_00_TO_90_LABELS = ["10","20","30","40","50","60","70","80","90","00"];
+const D_00_TO_90_LABELS = ["00","10","20","30","40","50","60","70","80","90"];
 
 function addZeroBasedPreset(dice3d, { type, labels, values, system, shape }) {
   dice3d.addDicePreset(
@@ -27,7 +27,7 @@ export function registerDiceSoNice() {
 
     // scripts/dice-so-nice.js
     addZeroBasedPreset(dice3d, { type: "dx", labels: D_0_TO_9_LABELS, values: {min:0,max:9}, system: system.id, shape: "d10" });
-    addZeroBasedPreset(dice3d, { type: "dh", labels: D_00_TO_90_LABELS, values: {min:0,max:99}, system: system.id, shape: "d100" });
+    addZeroBasedPreset(dice3d, { type: "dh", labels: D_00_TO_90_LABELS, values: {min:0,max:90}, system: system.id, shape: "d100" });
     addZeroBasedPreset(dice3d, { type: "dv", labels: D_0_TO_4_LABELS, values: {min:0,max:4}, system: system.id, shape: "d10" });
     
   });
